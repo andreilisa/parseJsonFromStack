63f6d446a65cfc6a5fdf6b93e331187d5146ecb2
andrei.lisa <andreilisa2001@gmail.com>
2021-10-04 10:02:41 +0300

    jsonParser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,95 +1,48 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-
-
-
-
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.*;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
-    public static void findAllKeys(Object object, Set<String> finalKeys) {
-          
-        if (object instanceof JSONObject) {
-            JSONObject jsonObject = (JSONObject) object;
+    public static void main(String[] args) throws FileNotFoundException {
 
-            jsonObject.keySet().forEach(chillKey -> {
-                System.out.println( chillKey + ":" + jsonObject.get(chillKey));
-                finalKeys.add(chillKey);
-                findAllKeys(jsonObject.get(chillKey), finalKeys);
-
-            });
-        } else if (object instanceof JSONArray) {
-            JSONArray jsonArray = (JSONArray) object;
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                Object o = jsonArray.get(i);
-                findAllKeys(o, finalKeys);
-            }
-        }
-
-
-    }
-    public List<Object> readJsonStream(InputStream in) throws IOException {
-        Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
-        List<Object> messages = new ArrayList<Object>();
-        reader.beginArray();
-        while (reader.hasNext()) {
-            Object message = gson.fromJson(reader, Object.class);
-            messages.add(message);
-        }
-        reader.endArray();
-        reader.close();
-        return messages;
-    }
-
-    public static void main(String[] args) throws JSONException, IOException {
-
-        String inputJson = "{\n" +
-                "  \"company\": {\n" +
-                "    \"employee\": {\n" +
-                "      \"person\": [\n" +
-                "        {\n" +
-                "          \"name\": \"Jack\",\n" +
-                "          \"secondName\": \"ADA\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"name\": \"Max\",\n" +
-                "          \"secondName\": \"ADA\"\n" +
-                "        }\n" +
-                "      ]\n" +
-                "    },\n" +
-                "    \"address\": \"address\",\n" +
-                "    \"phone\": \"number\"\n" +
-                "  }\n" +
-                "}\n" +
-                "  \n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "\n";
-
-        JSONObject jsonObject = new JSONObject(inputJson);
-        Set<String> finalKeys = new HashSet<>();
-        findAllKeys(jsonObject, finalKeys);
-        for (int i = 0; i < jsonObject.length(); i++){
-
-        }
-        Main main = new Main();
-        List<Object> list =  main.readJsonStream(new FileInputStream("C:\\Users\\andrei.lisa\\IdeaProjects\\JsonParser\\src\\main\\resources\\json\\file.json"));
-         for (int i =0; i < list.size(); i ++){
-
-         }
-
-
+        String jsonStr = "C:\\Users\\andrei.lisa\\IdeaProjects\\JsonParser\\src\\main\\resources\\json\\file.json";
+        JsonParser parser = new JsonParser();
+        JsonElement jsonElement = parser.parse(new FileReader(jsonStr));
+        processJsonElement(jsonElement);
     }
 
 
+    public static void processJsonElement(JsonElement e) {
+        if (e.isJsonArray()) {
+            processJsonArray(e.getAsJsonArray());
+        } else if (e.isJsonObject()) {
+            processJsonObject(e.getAsJsonObject());
+        }
+    }
 
+    public static void processJsonArray(JsonArray array) {
+        for (JsonElement value : array) {
+            processJsonElement(value);
+        }
+    }
+
+
+    public static void processJsonObject(JsonObject object) {
+        Set<Map.Entry<String, JsonElement>> members = object.entrySet();
+        for (Map.Entry<String, JsonElement> value : members) {
+            System.out.println(value.getKey() + ": " + value.getValue());
+
+            processJsonElement(value.getValue());
+        }
+    }
 }
+
+
+
+
